398dccf40360cd67145c17b5ff43fcbf66a87935
Integralist <mark.mcdx@gmail.com>
2013-10-06 21:59:25 +0100

    Switch over to CabinJS from Jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,128 @@
 module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-
-        imageDirectory: 'Assets/Images/src/',
-
-        // grunt responsive_images:dev:default
-        // grunt responsive_images:dev:lazyload
-        responsive_images: {
-            dev: {
-                options: {
-                    sizes: [
-                        {
-                            width: 320
-                        },
-                        {
-                            width: 480
-                        },
-                        {
-                            width: 640
-                        },
-                        {
-                            width: 760
-                        },
-                        {
-                            width: 920
-                        },
-                        {
-                            width: 1024
-                        }
-                    ]
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= imageDirectory %>',
-                    src: ['*.{jpg,gif,png}'],
-                    dest: '<%= imageDirectory %>generated/'
-                }]
-            }
+  grunt.initConfig({
+    'gh-pages': {
+        options: {
+            base: 'dist'
+        },
+        src: ['**']
+    },
+    pages: {
+      posts: {
+        src: 'posts',
+        dest: 'dist',
+        layout: 'src/layouts/post.ejs',
+        url: 'posts/:title/',
+        options: {
+          pageSrc: 'src/pages',
+          data: {
+            baseUrl: '/'
+          },
+          pagination: {
+            postsPerPage: 2,
+            listPage: 'src/pages/index.ejs'
+          }
         }
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'src/styles',
+          cssDir: 'dist/styles'
+        }
+      }
+    },
+    // Move files not handled by other tasks
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'src',
+          dest: 'dist',
+          src: [
+            'images/**',
+            'scripts/**',
+            'styles/**.css',
+            'styles/fonts/**',
+          ]
+        }]
+      }
+    },
+    watch: {
+      dist: {
+        files: ['dist/**'],
+        options: {
+          livereload: true
+        }
+      },
+      compass: {
+        files: ['src/styles/**'],
+        tasks: ['compass']
+      },
+      pages: {
+        files: [
+          'posts/**',
+          'src/layouts/**',
+          'src/pages/**'
+        ],
+        tasks: ['pages']
+      },
+      copy: {
+        files: [
+          'src/images/**',
+          'src/scripts/**',
+          'src/styles/**.css',
+          'src/styles/fonts/**'
+        ],
+        tasks: ['copy']
+      }
+    },
+    connect: {
+      dist: {
+        options: {
+        port: 5455,
+        hostname: '0.0.0.0',
+          middleware: function (connect) {
+            return [
+              require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
+              connect.static(require('path').resolve('dist'))
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      dist: {
+        path: 'http://localhost:5455'
+      }
+    },
+    clean: {
+      dist: 'dist'
+    }
+  });
 
-    });
+  grunt.registerTask('build', [
+    'clean',
+    'pages',
+    'compass',
+    'copy'
+  ]);
 
-    // Load NPM Tasks
-    grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('server', [
+    'build',
+    'connect',
+    'open',
+    'watch'
+  ]);
 
-    // Default Task
-    grunt.registerTask('default', ['responsive_images:dev']);
+  grunt.registerTask('default', 'server');
 
+  grunt.registerTask('deploy', [
+    'build',
+    'gh-pages'
+  ]);
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 };
---
+++ drafts/Ruby-SQLite-and-ActiveRecord.md
@@ -0,0 +1,19 @@
+{
+  title: "Ruby, SQLite and ActiveRecord",
+  date:  "2013-10-07",
+  description: "Most developers building sites with Ruby use the Ruby on Rails framework. But if you want to build a dynamic application using a more lightweight framework such as Sinatra then this post will show you how to incorporate the SQLite database and the ActiveRecord ORM which is available to use outside of Ruby On Rails."
+}
+
+## What we'll cover *reading time: approx. mins*
+
+- Introduction
+- x
+- Conclusion
+
+## Introduction
+
+tba
+
+## Conclusion
+
+tba
